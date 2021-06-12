859c0842ecb92066192fdc3638c19e7dffa6ba02
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-06-12 21:29:50 +0700

    packages

---
+++ src/views/PaketDombaJantan.vue
@@ -0,0 +1,139 @@
+<template>
+  <div
+    style="text-align:center; justify-content:center; margin:200px auto; background-color: white"
+  >
+    <div class="row" style="margin: 25px">
+        <div class="col" style="margin: 0 350px">
+            <h2><b>Paket Kurban Kambing / Domba Jantan</b></h2>
+            <br>
+            <br>
+            <div class="row justify-content-center align-content-around flex-wrap">
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/domba_jantan.png" alt="" width="150px">
+                                        <p>Berat: <b>25-40 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Kambing / Domba <span class="text-danger">Super</span></b></h5>
+                                        <h4><b>Rp 4.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/domba_jantan.png" alt="" width="150px">
+                                        <p>Berat: <b>25-40 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Kambing / Domba <span class="text-danger">Super</span></b></h5>
+                                        <h4><b>Rp 4.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/domba_jantan.png" alt="" width="150px">
+                                        <p>Berat: <b>25-40 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Kambing / Domba <span class="text-danger">Super</span></b></h5>
+                                        <h4><b>Rp 4.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/domba_jantan.png" alt="" width="150px">
+                                        <p>Berat: <b>25-40 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Kambing / Domba <span class="text-danger">Super</span></b></h5>
+                                        <h4><b>Rp 4.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* .card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+} */
+
+/* .price {
+  color: grey;
+  font-size: 22px;
+} */
+
+/* .card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+} */
+
+.card button:hover {
+  opacity: 0.7;
+}
+</style>
---
+++ src/views/PaketPatunganSapi.vue
@@ -0,0 +1,202 @@
+<template>
+  <div
+    style="text-align:center; justify-content:center; margin:200px auto; background-color: white"
+  >
+    <div class="row" style="margin: 25px">
+        <div class="col" style="margin: 0 350px">
+            <h2><b>Paket Kurban Kambing / Domba Jantan</b></h2>
+            <br>
+            <br>
+            <div class="row justify-content-center align-content-around flex-wrap">
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Kurban Sapi <span class="text-danger">Mandiri</span></b></h5>
+                                        <h4><b>Rp 17.500.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Patungan Kurban <span class="text-danger">2 Orang</span></b></h5>
+                                        <h4><b>Rp 15.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Patungan Kurban <span class="text-danger">3 Orang</span></b></h5>
+                                        <h4><b>Rp 12.500.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Patungan Kurban <span class="text-danger">4 Orang</span></b></h5>
+                                        <h4><b>Rp 10.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Patungan Kurban <span class="text-danger">5 Orang</span></b></h5>
+                                        <h4><b>Rp 7.500.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Patungan Kurban <span class="text-danger">6 Orang</span></b></h5>
+                                        <h4><b>Rp 5.000.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6" style="margin: 20px auto">
+                    <div class="d-flex justify-content-center">
+                        <div class="card" style="padding: 0 10px;">
+                            <div class="row">
+                                <div class="col d-flex align-items-center justify-content-end" style="padding: 0">
+                                    <div class="d-flex flex-column" >
+                                        <img src="../../public/img/patungan_sapi.png" alt="" width="150px">
+                                        <p>Berat: <b>200 Kg</b></p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex align-items-center justify-content-center" >
+                                    <div>
+                                        <h5><b>Patungan Kurban <span class="text-danger">7 Orang</span></b></h5>
+                                        <h4><b>Rp 2.500.000</b></h4>
+                                        <b-button pill variant="danger"><b>Kurban Sekarang</b></b-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* .card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+} */
+
+/* .price {
+  color: grey;
+  font-size: 22px;
+} */
+
+/* .card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+} */
+
+.card button:hover {
+  opacity: 0.7;
+}
+</style>
