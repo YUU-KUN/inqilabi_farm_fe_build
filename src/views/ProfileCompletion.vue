ca48b3ab7cd4a2c1f0a2d0f2682a4f885e4a32a9
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-06-12 21:30:16 +0700

    profile completion page

---
+++ src/views/ProfileCompletion.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+        <div class="container-scroller">
+          <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="main-panel">
+              <div class="content-wrapper d-flex align-items-center ">
+                <div class="row w-100">
+                  <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-center p-5">
+                      <h4>Lengkapi Profil</h4>
+                      <br>
+                      <form>
+                          <b-form-group>
+                            <b-form-input id="nama" v-model="nama" placeholder="Nama" trim required></b-form-input>
+                          </b-form-group>
+                          
+                          <b-form-group>
+                            <b-form-input id="nomor_wa" v-model="noWa" placeholder="No. Wa" trim required></b-form-input>
+                          </b-form-group>
+
+                          <b-form-group>
+                            <b-form-input id="alamat" v-model="alamat" placeholder="Alamat" trim required></b-form-input>
+                          </b-form-group>
+
+                          <b-form-group>
+                            <b-form-input id="kecamatan" v-model="kecamatan" placeholder="Kecamatan" trim required></b-form-input>
+                          </b-form-group>
+                          
+                          <b-form-group>
+                            <b-form-input id="kota" v-model="kota" placeholder="Kota" trim required></b-form-input>
+                          </b-form-group>
+
+                          <b-form-group>
+                            <b-form-input id="provinsi" v-model="provinsi" placeholder="Provinsi" trim required></b-form-input>
+                          </b-form-group>
+                        </form>
+                        <br>
+                        <b-button
+                          @click="profileCompletion"
+                          variant="primary"
+                          type="button"
+                          >Lanjutkan
+                        </b-button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- content-wrapper ends -->
+          </div>
+            <b-toast id="loadingToast" title="Processing Data" no-auto-hide>
+                <b-spinner label="Spinning" variant="success"></b-spinner>
+                <strong class="text-success">Loading...</strong>
+            </b-toast>
+            <!-- toast untuk tampilan message box -->
+            <b-toast id="message" title="Message">
+            <strong class="text-success">{{ message }}</strong>
+            </b-toast>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+      return {
+        nama: '',
+        noWa: '',
+        alamat: '',
+        kecamatan: '',
+        kota: '',
+        provinsi: '',
+        message: '',
+      }
+    },
+    methods: {
+        profileCompletion(){
+            this.$bvToast.show("loadingToast")
+            const form = {
+              nama: this.nama,
+              noWa: this.noWa,
+              alamat: this.alamat,
+              kecamatan: this.kecamatan,
+              kota: this.kota,
+              provinsi: this.provinsi
+            }
+            this.axios.post('')
+            .then((response) => {
+                this.message = response.data.message
+                this.$bvToast.hide("loadingToast")
+                this.$bvToast.show("message")
+                this.$router.push('/')
+            })
+            .catch(err => console.log(err))
+        },
+    },
+}
+</script>
