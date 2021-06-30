253d31a5f8f1e72a2182a4544067947702f45a31
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-06-30 15:57:33 +0700

    routing

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,23 @@ import Login from '../views/auth/Login.vue'
 import Register from '../views/auth/Register.vue'
 // import RegisterPhone from '../views/auth/RegisterPhone.vue'
 
-import Navbar from '../views/layouts/Navbar.vue'
+// import Navbar from '../views/layouts/Navbar.vue'
 import Header from '../views/layouts/Header.vue'
 import Footer from '../views/layouts/Footer.vue'
 
 import ProfileCompletion from '../views/ProfileCompletion.vue'
+import ChooseMorePackage from '../views/ChooseMorePackage.vue'
 import ChoosePackage from '../views/ChoosePackage.vue'
-import PaketDombaJantan from '../views/PaketDombaJantan.vue'
-import PaketPatunganSapi from '../views/PaketPatunganSapi.vue'
+import TransactionSuccess from '../views/TransactionSuccess.vue'
+// import PaketDombaJantan from '../views/PaketDombaJantan.vue'
+// import PaketPatunganSapi from '../views/PaketPatunganSapi.vue'
+// import UploadBukti from '../views/UploadBukti.vue'
 import Checkout from '../views/Checkout.vue'
-import ConfirmCheckout from '../views/ConfirmCheckout.vue'
-import UploadBukti from '../views/UploadBukti.vue'
+// import ConfirmCheckout from '../views/ConfirmCheckout.vue'
 import Pembayaran from '../views/Pembayaran.vue'
 import Report from '../views/Report.vue'
+import KurbanSaya from '../views/Kurban Saya.vue'
+
 import ReportAdmin from '../views/Report Admin.vue'
 import ReportDetail from '../views/Report Detail.vue'
 import ReportCreate from '../views/Report Create.vue'
@@ -47,35 +51,44 @@ const routes = [
       requiresAuth: true
     }
   },
+  // {
+  //   path: '/reportCreate',
+  //   name: 'Report Create',
+  //   components: {default: ReportCreate, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "Report Create",
+  //     requiresAuth: true
+  //   }
+  // },
+  // {
+  //   path: '/reportDetail',
+  //   name: 'Report Detail',
+  //   components: {default: ReportDetail, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "Report Detail",
+  //     requiresAuth: true
+  //   }
+  // },
+  // {
+  //   path: '/reportAdmin',
+  //   name: 'Report Admin',
+  //   components: {default: ReportAdmin, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "Report Admin",
+  //     requiresAuth: true
+  //   }
+  // },
   {
-    path: '/reportCreate',
-    name: 'Report Create',
-    components: {default: ReportCreate, header: Header, footer: Footer},
+    path: '/kurbanSaya',
+    name: 'Kurban Saya',
+    components: {default: KurbanSaya, header: Header, footer: Footer},
     meta: {
-      title: "Report Create",
+      title: "Kurban Saya",
       requiresAuth: true
     }
   },
   {
-    path: '/reportDetail',
-    name: 'Report Detail',
-    components: {default: ReportDetail, header: Header, footer: Footer},
-    meta: {
-      title: "Report Detail",
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/reportAdmin',
-    name: 'Report Admin',
-    components: {default: ReportAdmin, header: Header, footer: Footer},
-    meta: {
-      title: "Report Admin",
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/report',
+    path: '/report/:id_pembayaran',
     name: 'Report',
     components: {default: Report, header: Header, footer: Footer},
     meta: {
@@ -92,24 +105,24 @@ const routes = [
       requiresAuth: true
     }
   },
-  {
-    path: '/uploadBukti',
-    name: 'UploadBukti',
-    components: {default: UploadBukti, header: Header, footer: Footer},
-    meta: {
-      title: "UploadBukti",
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/confirmCheckout',
-    name: 'ConfirmCheckout',
-    components: {default: ConfirmCheckout, header: Header, footer: Footer},
-    meta: {
-      title: "ConfirmCheckout",
-      requiresAuth: true
-    }
-  },
+  // {
+  //   path: '/uploadBukti',
+  //   name: 'UploadBukti',
+  //   components: {default: UploadBukti, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "UploadBukti",
+  //     requiresAuth: true
+  //   }
+  // },
+  // {
+  //   path: '/confirmCheckout',
+  //   name: 'ConfirmCheckout',
+  //   components: {default: ConfirmCheckout, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "ConfirmCheckout",
+  //     requiresAuth: true
+  //   }
+  // },
   {
     path: '/checkout',
     name: 'Checkout',
@@ -119,21 +132,39 @@ const routes = [
       requiresAuth: true
     }
   },
+  // {
+  //   path: '/paketPatunganSapi',
+  //   name: 'Paket Patungan Sapi',
+  //   components: {default: PaketPatunganSapi, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "Paket Patungan Sapi",
+  //     requiresAuth: true
+  //   }
+  // },
+  // {
+  //   path: '/paketDombaJantan',
+  //   name: 'Paket Domba Jantan',
+  //   components: {default: PaketDombaJantan, header: Header, footer: Footer},
+  //   meta: {
+  //     title: "Paket Domba Jantan",
+  //     requiresAuth: true
+  //   }
+  // },
   {
-    path: '/paketPatunganSapi',
-    name: 'Paket Patungan Sapi',
-    components: {default: PaketPatunganSapi, header: Header, footer: Footer},
+    path: '/transactionSuccess',
+    name: 'Transaction Success',
+    components: {default: TransactionSuccess, header: Header},
     meta: {
-      title: "Paket Patungan Sapi",
+      title: "Transaction Success",
       requiresAuth: true
     }
   },
   {
-    path: '/paketDombaJantan',
-    name: 'Paket Domba Jantan',
-    components: {default: PaketDombaJantan, header: Header, footer: Footer},
+    path: '/choosePackage/:type',
+    name: 'Choose More Packages',
+    components: {default: ChooseMorePackage, header: Header, footer: Footer},
     meta: {
-      title: "Paket Domba Jantan",
+      title: "Choose More Packages",
       requiresAuth: true
     }
   },
@@ -158,7 +189,7 @@ const routes = [
   {
     path: '/home',
     name: 'home',
-    components: {default: Home, header: Navbar, footer: Footer},
+    components: {default: Home, header: Header, footer: Footer},
     meta: {
       title: "Home",
       requiresAuth: true
@@ -172,16 +203,16 @@ const router = new VueRouter({
   routes: routes
 })
 
-// router.beforeEach((to, from, next) => {
-//   if(to.matched.some(record => record.meta.requiresAuth)) {
-//     if (store.getters.isLoggedIn) {
-//       next()
-//       return
-//     }
-//     next('/login') 
-//   } else {
-//     next() 
-//   }
-// })
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
+})
 
 export default router
