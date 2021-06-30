56653941694364f2a02ea254c4660f45c2569dde
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-06-30 15:58:39 +0700

    import packages

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueCookies from 'vue-cookies'
+import VueCurrency from 'vue-currency-filter'
 import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
+import moment from 'moment'
 import App from './App.vue'
+
 //load CSS
 import '../public/css/materialdesignicons.min.css'
 import '../public/css/vendor.bundle.base.css'
@@ -21,8 +24,29 @@ Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.use(VueCookies)
 Vue.use(VueAxios, axios)
+Vue.use(VueCurrency, {
+  name: 'rupiah',
+  symbol : 'Rp',
+  thousandsSeparator: '.',
+  fractionCount: 2,
+  fractionSeparator: ',',
+  symbolPosition: 'front',
+  symbolSpacing: true,
+  avoidEmptyDecimals: undefined,
+})
+
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).locale('id').format('D MMM YYYY') 
+  }
+})
+
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://localhost:8001/api'
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+if (localStorage.getItem("Authorization")) {
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem("Authorization")
+}
 
 new Vue({
   router,
@@ -31,7 +55,7 @@ new Vue({
     isAuthenticate : function(){
       if(localStorage.getItem("Authorization")){
         let conf = { headers : {"Authorization" : "Bearer " + localStorage.getItem("Authorization")} };
-        this.axios.get("/login/check", conf)
+        this.axios.get("/profile", conf)
         .then(response => {
           if(response.data.auth == false || response.data.status == 0){
             this.$store.commit('logout')
